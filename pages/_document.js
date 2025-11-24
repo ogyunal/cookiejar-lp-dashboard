51d96b378be19def69038b09098be5b258c5c589
Oğulcan Ünal <ogulcan@ogyunal.com>
2025-11-24 12:37:49 +0300

    datafast analytics

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,22 @@ export default function Document() {
         <link rel="icon" href="/images/icon.png" />
         <link rel="apple-touch-icon" href="/images/icon.png" />
         <meta name="theme-color" content="#D4A574" />
+        
+        {/* DataFast Analytics - Main Site */}
+        <script
+          defer
+          data-website-id="dfid_t5ZcGWP4cfNCt3YDrggni"
+          data-domain="thecookiejar.app"
+          src="https://datafa.st/js/script.js"
+        />
+        
+        {/* DataFast Analytics - Creator Dashboard */}
+        <script
+          defer
+          data-website-id="dfid_a2hJ9XvOxMZRLEycvaaLo"
+          data-domain="creator.thecookiejar.app"
+          src="https://datafa.st/js/script.js"
+        />
       </Head>
       <body>
         <Main />
